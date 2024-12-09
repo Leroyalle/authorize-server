3494363d57f22ffd45507df63b695baf8daa0635
Leroyalle <nikolajmelonov@yandex.ru>
2024-12-10 02:18:27 +0300

    feat: add authorized decorator and findProfile controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common'
+import { Authorization } from 'src/auth/decorators/auth.decorator'
+import { Authorized } from 'src/auth/decorators/authorized.decorator'
 
 import { UserService } from './user.service'
 
 @Controller('users')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
+
+	@Authorization()
+	@Get('/profile')
+	@HttpCode(HttpStatus.OK)
+	public async findProfile(@Authorized('id') userId: string) {
+		return this.userService.findById(userId)
+	}
 }

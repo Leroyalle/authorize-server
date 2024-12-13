6cd12073adfb76b6a4a1743c9419d20672c926cb
Leroyalle <nikolajmelonov@yandex.ru>
2024-12-13 18:38:01 +0300

    feat: add provider guard and connect controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,32 @@
 import {
 	Body,
 	Controller,
+	Get,
 	HttpCode,
 	HttpStatus,
+	Param,
 	Post,
 	Req,
-	Res
+	Res,
+	UseGuards
 } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Recaptcha } from '@nestlab/google-recaptcha'
 import { Request, Response } from 'express'
 
 import { AuthService } from './auth.service'
 import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
+import { AuthProviderGuard } from './guards/provider.guard'
+import { ProviderService } from './provider/provider.service'
 
 @Controller('auth')
 export class AuthController {
-	public constructor(private readonly authService: AuthService) {}
+	public constructor(
+		private readonly authService: AuthService,
+		private readonly providerService: ProviderService,
+		private readonly configService: ConfigService
+	) {}
 
 	@Recaptcha()
 	@Post('register')
@@ -30,6 +40,16 @@ export class AuthController {
 	@HttpCode(HttpStatus.OK)
 	public async login(@Req() req: Request, @Body() dto: LoginDto) {
 		return this.authService.login(req, dto)
+	}
+
+	@UseGuards(AuthProviderGuard)
+	@Get('oauth/connect:provider')
+	@HttpCode(HttpStatus.OK)
+	public async connect(@Param('provider') provider: string) {
+		const providerInstance = this.providerService.findByService(provider)
+		return {
+			url: providerInstance.getAuthUrl()
+		}
 	}
 
 	@Post('logout')
